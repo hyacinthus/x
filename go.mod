238641f2a438f4b2b8d678a0574c0fec9c291f19
Muninn <hyacinthus@gmail.com>
2018-12-18 12:03:59 +0800

    add model

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/mozillazg/go-cos v0.10.0
 	github.com/nsqio/go-nsq v1.0.7
+	github.com/rs/xid v1.2.1
 	github.com/sirupsen/logrus v1.2.0
 	github.com/tencentyun/cos-go-sdk-v5 v0.0.0-20181213100331-bd54f9e1350e
 	github.com/vmihailenco/msgpack v4.0.1+incompatible
---
+++ model/model.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/rs/xid"
+)
+
+// Entity 实体共用字段
+type Entity struct {
+	// ID xid 20位小写字符串全局id
+	ID string `json:"id" gorm:"type:varchar(20);primary_key"`
+	// 创建时间
+	CreatedAt time.Time `json:"created_at"`
+	// 最后更新时间
+	UpdatedAt time.Time `json:"updated_at"`
+	// 软删除
+	DeletedAt *time.Time `json:"-"`
+}
+
+// BeforeCreate GORM hook
+func (*Entity) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("ID", xid.New().String())
+	return nil
+}
+
+// Log 日志共用字段
+type Log struct {
+	// ID xid 20位小写字符串全局id
+	ID string `json:"id" gorm:"type:varchar(20);primary_key"`
+	// 创建时间
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// BeforeCreate GORM hook
+func (*Log) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("ID", xid.New().String())
+	return nil
+}
