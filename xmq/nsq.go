9f841629eafc777623b853ae775230798cc4e985
Muninn <hyacinthus@gmail.com>
2019-06-03 15:45:57 +0800

    fix create topic

--- xmq/nsq.go
+++ xmq/nsq.go
@@ -99,7 +99,7 @@ func (c *nsqClient) Delay(topic string, payload interface{}, delay time.Duration
 // CreateTopic create a topic on nsqd by http request
 func (c *nsqClient) CreateTopic(topic string) error {
 	resp, err := grequests.Post("http://"+c.config.PubHost+":"+c.config.PubHTTP+"/topic/create",
-		&grequests.RequestOptions{QueryStruct: map[string]string{"topic": topic}})
+		&grequests.RequestOptions{Params: map[string]string{"topic": topic}})
 	if err != nil {
 		log.WithError(err).Errorf("创建主题 %s 出错", topic)
 		return err
